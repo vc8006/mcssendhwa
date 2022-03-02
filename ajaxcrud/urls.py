c1383709b32c08b2c325d25ceb31ef55971f1832
vc8006 <vedchourasia08@gmail.com>
2022-03-03 01:53:34 +0530

    add

--- ajaxcrud/urls.py
+++ ajaxcrud/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +33,4 @@ urlpatterns = [
     path('generatebill/<int:id>', views.generatebill,name="generatebill"),
     path('generatebillold/<int:id>/<int:billno>', views.download_old,name="download_old"),
     # path('generatepdf/<int:id>/', views.Generatepdf.as_view(), name = 'generatepdf'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
