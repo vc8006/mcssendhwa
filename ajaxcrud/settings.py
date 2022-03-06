6771feb4723f34d1cbc1dacba99e5febc5383bc6
vc8006 <65337236+vc8006@users.noreply.github.com>
2022-03-06 11:10:40 +0530

    Update settings.py
    added allowed host

--- ajaxcrud/settings.py
+++ ajaxcrud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-czz$bbbu+w%gps67bi=%(ihjjmn2_-g_f&0co*!j4_g!q!vt%j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mcssendhwa.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['mcssendhwa.herokuapp.com','localhost','44.201.196.113']
 
 
 # Application definition
